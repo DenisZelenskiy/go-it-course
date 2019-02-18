f0d5ab210b8194936bf4955a088c69c284992b53
Denis Zelenskiy <den.zelenskiy@gmail.com>
2019-02-18 19:34:15 +0200

    add HW 7

---
+++ homework-01-07/JS/script.js
@@ -0,0 +1,76 @@
+'use strict';
+
+/*
+  1. Модифицируйте готовую функцию createPostCard() из задания 
+    номер 6 (https://codepen.io/goit-fe-adv/pen/MVPaeZ) так, 
+    чтобы она принимала объект post с данными для заполнения полей 
+    в карточке.
+      
+  2. Создайте функцию createCards(posts), которая принимает массив
+    объектов-карточек, вызывает функцию createPostCard(post) столько
+    раз, сколько объектов в массиве, сохраняя общий результат и возвращает 
+    массив DOM-элементов всех постов.
+    
+  3. Повесьте все посты в какой-то уже существующий DOM-узел.
+*/
+
+// 
+
+
+const posts = [
+    {
+        img: "https://placeimg.com/400/150/arch",
+        alt: "pic-1",
+        title: "Post title 1",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+        link: 'link-1.com'
+    },
+    {
+        img: "https://placeimg.com/400/150/nature",
+        alt: "pic-2",
+        title: "Post title 2",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+        link: 'link-2.com'
+    },
+    {
+        img: "https://placeimg.com/400/150/arch",
+        alt: "pic-3",
+        title: "Post title 3",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+        link: 'link-3.com'
+    }
+];
+
+
+function createPostCard(posts) {
+    const body = document.body;
+    const list = document.createElement('div')
+    const img = document.createElement('img')
+    const title = document.createElement('h2')
+    const text = document.createElement('p')
+    const link = document.createElement('a')
+
+    list.classList.add('post')
+
+    img.classList.add('post__image')
+    img.src = posts.img
+    img.alt = posts.alt
+
+    title.classList.add('post__title')
+    title.textContent = posts.title
+
+    text.classList.add('post__text')
+    text.textContent = posts.text
+
+    link.classList.add('button')
+    link.href = posts.link
+    link.textContent = "Read more"
+
+    body.appendChild(list).append(img, title, text, link)
+}
+
+const createCards = (posts) => posts.forEach(el => createPostCard(el));
+
+createCards(posts);
+
+console.log(document.body)
